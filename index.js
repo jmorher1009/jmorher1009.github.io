c2069dacfa5200a21321b5a2bb901355f59fc68b
chema <72257361+jmorher1009@users.noreply.github.com>
2023-08-25 19:29:53 +0200

    -.

--- index.js
+++ index.js
@@ -1,41 +1,5 @@
-let select = document.querySelector("select");
-let html = document.querySelector("html");
+document.getElementById("submit").addEventListener("click", function(){
 
-function cambiatema(bgcolor,textcolor){
-
-    html.style.backgroundColor = bgcolor;
-    html.style.color = textcolor;
-}
-
-select.onchange = function(){
-
-    switch(select.value){
-        case("negro"):
-        {
-            cambiatema("black","white");
-            break;
-        }
-        case("blanco"):{
-            cambiatema("white","black");
-            break;
-        }
-        case("morado"):{
-            cambiatema("purple","black");
-            break;
-        }
-        case("amarillo"):{
-            cambiatema("yellow","black");
-            break;
-        }
-        case("rojo"):{
-            cambiatema("red","black");
-            break;
-        }
-        
-    }
-
-    /*select.value === "negro" ? 
-    cambiatema("black","white") 
-    : cambiatema("white","black");
-*/
-}; 
+  alert("hola");
+});
+  
