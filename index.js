ba5a29ad249bbb498b9a0fd2f4d6b6926748d248
chema <72257361+jmorher1009@users.noreply.github.com>
2024-03-08 13:20:15 +0100

    .

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ arr.forEach(button => {
             string = string.substring(0, string.length - 1);
             input.value = string;
         }
+        
         else{
             if(e.target.innerHTML == '()'){
                 if(!parentesis){
